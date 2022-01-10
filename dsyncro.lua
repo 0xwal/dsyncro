03deda96652155a1be8d9a39e126826f0ff92b22
0xWaleed <imwaleed@outlook.sa>
2022-01-10 09:58:04 +0300

    feat: support nested direct tables ✨ 🚧

--- dsyncro.lua
+++ dsyncro.lua
@@ -3,16 +3,29 @@
 --- DateTime: 30/12/2021 3:54 PM
 ---
 
-local dsyncroMT      = {}
-dsyncro              = {}
+local dsyncroMT   = {}
+dsyncro           = {}
 
-dsyncroMT.__index    = function(t, k)
+dsyncroMT.__index = function(t, k)
     return t.__store[k]
 end
 
-dsyncroMT.__newindex = function(t, key, value)
+local function invoke_watcher_recursively(t, k, v)
+    local w = t.__watchers[k]
 
-    if key:find('@') then
+    if w then
+        w(v)
+    end
+
+    if not t.__parent then
+        return
+    end
+
+    invoke_watcher_recursively(t.__parent, t.__parentName, t.__parent.__store[t.__parentName])
+end
+
+dsyncroMT.__newindex = function(t, key, value)
+    if type(key) == 'string' and key:find('@') then
         local actualKey         = string.gsub(key, '@', '')
         t.__watchers[actualKey] = value
         return
@@ -24,23 +37,33 @@ dsyncroMT.__newindex = function(t, key, value)
         key = string.gsub(key, '-', '')
     end
 
-    local watcher  = t.__watchers[key]
+    if type(value) ~= 'table' then
+        if type(key) == 'number' then
+            table.insert(t.__store, value)
+        else
+            t.__store[key] = value
+        end
+    else
+        local newT = dsyncro.new()
+        for k, v in pairs(value) do
+            newT[k] = v
+        end
 
-    local oldValue = t.__store[key]
+        rawset(newT, '__parent', t)
+        rawset(newT, '__parentName', key)
 
-    if oldValue == value then
-        return
+        if type(key) == 'number' then
+            table.insert(t.__store, value)
+        else
+            t.__store[key] = newT
+        end
     end
-
-    t.__store[key] = value
 
     if not shouldBeSilent then
         t:_invokeSetCallbacks(key, value)
     end
 
-    if watcher then
-        watcher(oldValue, value)
-    end
+    invoke_watcher_recursively(t, key, value)
 end
 
 function dsyncroMT:onKeySet(callback)
--- spec/dsyncro_spec.lua
+++ spec/dsyncro_spec.lua
@@ -9,6 +9,47 @@ local function hard_require(module)
     return m
 end
 
+local function internal_compare(value, expected, iterator)
+    local hasError = false
+    for k, _ in iterator(expected) do
+        local expectedValue = expected[k]
+        local actualValue   = value[k]
+        if type(expectedValue) == 'table' then
+            if type(actualValue) ~= 'table' then
+                hasError = false
+            else
+                hasError = internal_compare(actualValue, expectedValue, iterator)
+            end
+        else
+            if not value[k] or value[k] ~= expected[k] then
+                return false
+            end
+        end
+
+        if hasError then
+            return false
+        end
+    end
+    return true
+end
+
+local function object_contain(_, args)
+    local expected = args[1]
+    return function(value)
+        return internal_compare(value, expected, pairs)
+    end
+end
+
+local function array_contain(_, args)
+    local expected = args[1]
+    return function(value)
+        return internal_compare(value, expected, ipairs)
+    end
+end
+
+assert:register('matcher', 'object_contain', object_contain)
+assert:register('matcher', 'array_contain', array_contain)
+
 describe('dsyncro', function()
 
     before_each(function()
@@ -34,11 +75,66 @@ describe('dsyncro', function()
         local watcherSpy = spy()
         dsyncro['@name'] = watcherSpy
         dsyncro['name']  = 'waleed'
-        assert.spy(watcherSpy).was_called_with(nil, 'waleed')
+        assert.spy(watcherSpy).was_called_with('waleed')
         dsyncro['name'] = 'Waleed'
-        assert.spy(watcherSpy).was_called_with('waleed', 'Waleed')
+        assert.spy(watcherSpy).was_called_with('Waleed')
 
         assert.spy(watcherSpy).was_called(2)
+    end)
+
+    it('should able to invoke watcher for a table', function()
+        local dsyncro        = dsyncro.new()
+        local watcherSpy     = spy()
+        dsyncro['@students'] = watcherSpy
+        dsyncro['students']  = {}
+        assert.spy(watcherSpy).was_called_with(match.object_contain({}))
+        dsyncro['students']['Waleed'] = true
+        assert.spy(watcherSpy).was_called_with(match.object_contain({ Waleed = true }))
+        dsyncro['students']['BISOON'] = true
+        assert.spy(watcherSpy).was_called_with(match.object_contain({ Waleed = true, BISOON = true }))
+        assert.spy(watcherSpy).was_called(3)
+    end)
+
+    it('should be able to invoke watcher for a table that got a new value inserted', function()
+        local dsyncro        = dsyncro.new()
+        local watcherSpy     = spy()
+        dsyncro['@students'] = watcherSpy
+        dsyncro['students']  = {}
+        assert.spy(watcherSpy).was_called_with(match.array_contain({}))
+        table.insert(dsyncro['students'], 'Waleed')
+        assert.spy(watcherSpy).was_called_with(match.array_contain({ 'Waleed' }))
+        table.insert(dsyncro['students'], 'BISOON')
+        assert.spy(watcherSpy).was_called_with(match.array_contain({ 'Waleed', 'BISOON' }))
+        assert.spy(watcherSpy).was_called(3)
+    end)
+
+    it('should be able to invoke watcher for multi nested table that got a new value inserted', function()
+        local dsyncro                = dsyncro.new()
+        local watcherSpy             = spy()
+        dsyncro['@class']            = watcherSpy
+        dsyncro['class']             = {}
+        dsyncro['class']['students'] = {}
+        assert.spy(watcherSpy).was_called_with(match.object_contain { class = { students = {} } })
+        table.insert(dsyncro['class']['students'], 'Waleed')
+        assert.spy(watcherSpy).was_called_with(match.object_contain { class = { students = { 'Waleed' } } })
+        table.insert(dsyncro['class']['students'], 'BISOON')
+        assert.spy(watcherSpy).was_called_with(match.object_contain { class = { students = { 'Waleed', 'BISOON' } } })
+        assert.spy(watcherSpy).was_called(4)
+    end)
+
+    it('should carry nested table key and value pairs as associative array', function()
+        local dsyncro        = dsyncro.new()
+        local watcherSpy     = spy()
+        dsyncro['@students'] = watcherSpy
+        dsyncro['students']  = {}
+        assert.spy(watcherSpy).was_called_with(match.array_contain({}))
+        table.insert(dsyncro['students'], { name = 'Waleed' })
+        assert.spy(watcherSpy).was_called_with(match.array_contain({ { name = 'Waleed' } }))
+        assert.is_equal('Waleed', dsyncro['students'][1].name)
+        table.insert(dsyncro['students'], { name = 'BISOON' })
+        assert.spy(watcherSpy).was_called_with(match.array_contain({ { name = 'Waleed' }, { name = 'BISOON' } }))
+        assert.is_equal('BISOON', dsyncro['students'][2].name)
+        assert.spy(watcherSpy).was_called(3)
     end)
 end)
 
@@ -52,16 +148,17 @@ describe('sync', function()
         assert.spy(onSetSpy).was_called_with('name', 'Waleed')
     end)
 
-    it('should not invoke when value is not changed', function()
-        local value    = 'Waleed'
-        local dsyncro  = dsyncro.new()
-        local onSetSpy = spy()
-        dsyncro:onKeySet(onSetSpy)
-        dsyncro['name'] = value
-        dsyncro['name'] = value
-        assert.spy(onSetSpy).was_called(1)
-        assert.spy(onSetSpy).was_called_with('name', 'Waleed')
-    end)
+    --todo required for later, we don't want to execute watcher for uncached value
+    --it('should not invoke when value is not changed', function()
+    --    local value    = 'Waleed'
+    --    local dsyncro  = dsyncro.new()
+    --    local onSetSpy = spy()
+    --    dsyncro:onKeySet(onSetSpy)
+    --    dsyncro['name'] = value
+    --    dsyncro['name'] = value
+    --    assert.spy(onSetSpy).was_called(1)
+    --    assert.spy(onSetSpy).was_called_with('name', 'Waleed')
+    --end)
 
     it('should able to set a value without invoking callback', function()
         local dsyncro  = dsyncro.new()
